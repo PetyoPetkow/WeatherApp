3e29dee12b10c7548c32c29cc69629f23d28e9c0
Petyo Petkov <Pspetkov13@gmail.com>
2022-12-06 17:03:52 +0200

    add logout

--- src/components/Header/MyHeader.js
+++ src/components/Header/MyHeader.js
@@ -5,8 +5,11 @@ import { Button, Space } from "antd";
 
 import { UserContext } from "../../UserContext";
 import SearchBar from "../SerachBar/SearchBar";
+import { signOut, getAuth } from "firebase/auth";
+import { auth } from "../../config/firebase";
 
 const MyHeader = ({ onSearchHandler }) => {
+    const auth = getAuth();
     const { user, setUser } = useContext(UserContext);
     return (
         <div>
@@ -18,22 +21,33 @@ const MyHeader = ({ onSearchHandler }) => {
             </NavLink>
             <SearchBar onSearchHandler={onSearchHandler}></SearchBar>
             {user ? (
-                <div
-                    style={{ color: "white", float: "right", fontSize: "20px" }}
-                >
-                    Hello, {user.user.displayName}
-                </div>
+                <Space style={{ float: "right" }} size={20}>
+                    <div
+                        style={{
+                            color: "white",
+                            float: "right",
+                            fontSize: "20px",
+                        }}
+                    >
+                        Hello, {user.user.displayName}
+                    </div>
+                    <Button
+                        onClick={() => signOut(auth).then(setUser())}
+                        type="default"
+                        ghost
+                    >
+                        logout
+                    </Button>
+                </Space>
             ) : (
-                <div style={{ float: "right" }}>
-                    <Space size={20}>
-                        <Button type="default" ghost>
-                            <NavLink to={"/login"}>login</NavLink>
-                        </Button>
-                        <Button type="default" ghost>
-                            <NavLink to={"/register"}>register</NavLink>
-                        </Button>
-                    </Space>
-                </div>
+                <Space style={{ float: "right" }} size={20}>
+                    <Button type="default" ghost>
+                        <NavLink to={"/login"}>login</NavLink>
+                    </Button>
+                    <Button type="default" ghost>
+                        <NavLink to={"/register"}>register</NavLink>
+                    </Button>
+                </Space>
             )}
         </div>
     );
